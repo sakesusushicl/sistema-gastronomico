6b0ceeacea1fc05cbb142f40313de49aacc57bce
sakesusushicl <sakesusushicl@gmail.com>
2026-08-22 19:31:11 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
